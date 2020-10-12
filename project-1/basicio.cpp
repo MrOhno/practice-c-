db403dce63a0fbbc65436ffe087bba2c15873654
MrOhno <phamtechlong@gmail.com>
2020-10-12 15:49:03 +0700

    added project-1

---
+++ project-1/basicio.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, world!" << "\n";
+	cout << "Hello" << "World" << endl;
+	cout << "Enter your age:" << endl;
+	int age;
+	cin >> age;
+	cout << "Your age is " << age << endl; 
+	return 0;
+}
+
